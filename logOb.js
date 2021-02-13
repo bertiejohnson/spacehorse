f1709578cf646eb7914cfeb2e1be6377e0f92717
bertiejohnson <darinbeckett63@gmail.com>
2021-02-13 07:50:04 +0000

    Added another teest function

--- logOb.js
+++ logOb.js
@@ -1,8 +1,11 @@
 export default {
-  info: "Test string",
+  info: "Test string Updated with new function",
   methods: {
     test: function(n) {
       return n * n
+    },
+    blah: function(str) {
+      return `You wanted this printed: ${str}`
     }
   }
 }
